6e83f5c6a43a4e49e2eaa35128d883903c089fac
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-14 01:46:23 -0500

    Configurando imports  y env db

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-const express = require('express');
-const morgan = require("morgan");
-const cors = require('cors');
-const {sequelize} = require('./database/db');
-const tasksRouts = require('./routes/tasks');
-const categoria = require('./models/categoria');
-const inventario = require('./models/inventario');
-const product = require('./models/product');
-
+import app from "./app.js";
+import { sequelize } from "./database/db.js";
 
 const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(tasksRouts)
-
 async function main() {
-    try {
-        await sequelize.sync();
-        console.log('Connection has been established successfully.');
-        app.listen(4000);
-        console.log('server on port 4000');
-    } catch (error) {
-        console.error('unable to conecte to database', error);
-    }
+  try {
+    await sequelize.sync();
+    console.log("Connection has been established successfully.");
+    app.listen(4000);
+    console.log("server on port 4000");
+  } catch (error) {
+    console.error("unable to conecte to database", error);
+  }
 }
 
 main();
