891b19e9e8dbc277e1c5d5ce4402139fdb37c299
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-14 02:58:38 -0500

    Initial routes for inventary

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import productsRoutes from "./routes/products.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
+import inventaryRoutes from "./routes/inventary.routes.js";
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use(productsRoutes);
+app.use(categoryRoutes);
+app.use(inventaryRoutes);
 export default app;
