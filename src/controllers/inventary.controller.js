f671677a26fd66d100ac4044d1b6256bcf641870
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-20 16:55:06 -0500

    Eliminando inventario

--- src/controllers/inventary.controller.js
+++
@@ -1,6 +0,0 @@
-import { Inventario } from "../models/inventario.js";
-import { Product } from "../models/Product.js";
-
-export const getCantidadTotal = async (re, res) => {};
-export const getCantidadProduct = async (req, res) => {};
-export const createInventary = async (res, req) => {};
