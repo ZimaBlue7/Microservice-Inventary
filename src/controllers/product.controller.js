3bacd09d258cb9f07b223d8549a17c0a01e6183d
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-28 22:49:20 -0500

    Nuevo prueba con actions

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,7 @@ import { Product } from "../models/Product.js";
 export const getProducts = async (req, res) => {
   try {
     const product = await Product.findAll();
-    res.json(product);
+    res.status(200).json(product);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
